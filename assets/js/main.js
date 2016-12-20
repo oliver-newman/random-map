e4d2bdf9e61f33897f51eded8b47085a491a1848
Oliver Newman <oliver@whmsi.com>
2016-12-19 21:56:16 -0500

    Implement pop-up info menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,37 @@ $(function () {
         lon = (Math.random() - 0.5) * 360,
         zoom = 7;
 
-  const googleMapsURL = `https://www.google.com/maps/@${lat},${lon},${zoom}z`
-  const imageURL = `https://api.mapbox.com/v4/digitalglobe.nal0g75k/${lon},` +
+  const googleMapsURL = `https://www.google.com/maps/@${lat},${lon},${zoom}z`,
+        imageURL = `https://api.mapbox.com/v4/digitalglobe.nal0g75k/${lon},` +
                   `${lat},${zoom}/${w}x${h}.png?access_token=${accessToken}`;
 
   $('body').css('background-image', `url(${imageURL})`);
   $('.google-maps-link').attr('href', googleMapsURL);
+
+  let mouseOverMenu = false;
+  let mouseOverIcon = false;
+
+  // Display menu when user hovers over the info icon
+  $('.info-button').hover(function () {
+    mouseOverIcon = true;
+    $('.info-box').slideDown();
+  }, function () {
+    mouseOverIcon = false;
+    setTimeout(function () { // TODO: not the best way to do this
+      if (!mouseOverMenu) {
+        $('.info-box').slideUp();
+      }
+    }, 200);
+  });
+
+  // Keep menu displayed while the user hovers over it
+  $('.info-box').hover(function () {
+    mouseOverMenu = true;
+  }, function () {
+    mouseOverMenu = false;
+    if (!mouseOverIcon) {
+      $('.info-box').slideUp();
+    }
+  });
 });
+
